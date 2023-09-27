b64943127c74f40f53e8e8bd64dc8c4da40699e1
tszlatkova <ztsvetomira@gmail.com>
2023-09-27 21:03:01 +0300

    Lecture 03 - Lists

---
+++ Fundamentals/03-PF-Lists-Basics/L01_strange_zoo.py
@@ -0,0 +1,19 @@
+# You are at the zoo, and the meerkats look strange.
+#
+# You will receive 3 strings on separate lines, representing the tail, the body, and the head of an animal in that
+# order. Your task is to re-arrange the elements in a list so that the animal looks normal again:
+#
+# · On the first position is the head;
+#
+# · On the second position is the body;
+#
+# · On the last one is the tail.
+
+input_list = []
+
+for _ in range(3):
+    input_list.append(input())
+
+input_list[0], input_list[2] = input_list[2], input_list[0]
+
+print(input_list)
