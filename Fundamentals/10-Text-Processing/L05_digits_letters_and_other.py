4a317bad3c5d98db89a3586b46b7120a9b81def6
tszlatkova <ztsvetomira@gmail.com>
2023-11-11 22:58:56 +0200

    Create L05_digits_letters_and_other.py

---
+++ Fundamentals/10-Text-Processing/L05_digits_letters_and_other.py
@@ -0,0 +1,17 @@
+# Write a program that receives a single string. On the first line, print all the digits found in the string,
+# on the second – all the letters, and on the third – all the other characters. There will always be at least one digit,
+# one letter, and one other character.
+
+input_string = input()
+
+digits, letters, others = '', '', ''
+
+for character in input_string:
+    if ord(character) in range(97, 123) or ord(character) in range(65, 91):
+        letters += character
+    elif ord(character) in range(48, 58):
+        digits += character
+    else:
+        others += character
+
+print(f'{digits}\n{letters}\n{others}')
