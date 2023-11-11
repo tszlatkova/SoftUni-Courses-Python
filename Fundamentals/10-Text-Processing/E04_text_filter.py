5b4c0644b97ba2fa1b68dd858ed32b66ce70e2b5
tszlatkova <ztsvetomira@gmail.com>
2023-11-11 22:51:53 +0200

    Update E04_text_filter.py

--- Fundamentals/10-Text-Processing/E04_text_filter.py
+++ Fundamentals/10-Text-Processing/E04_text_filter.py
@@ -6,8 +6,7 @@ banned_words = input().split(', ')
 text = input()
 
 for word in banned_words:
-    while word in text:
-        word_to_replace_with = '*' * len(word)
-        text = text.replace(word, word_to_replace_with)
+    word_to_replace_with = '*' * len(word)
+    text = text.replace(word, word_to_replace_with)
 
 print(text)
