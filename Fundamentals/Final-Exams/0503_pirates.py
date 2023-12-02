77c14bab08b5777d46ba1cbcf2796a507a65a417
tszlatkova <ztsvetomira@gmail.com>
2023-12-02 19:16:52 +0200

    Create 0503_pirates.py

---
+++ Fundamentals/Final-Exams/0503_pirates.py
@@ -0,0 +1,110 @@
+# Anno 1681. The Caribbean. The golden age of piracy. You are a well-known pirate captain by the name of Jack Daniels.
+# Together with your comrades Jim (Beam) and Johnny (Walker), you have been roaming the seas, looking for gold and
+# treasure… and the occasional killing, of course. Go ahead, target some wealthy settlements and show them the pirate's
+# way!
+# Until the "Sail" command is given, you will be receiving:
+# •	You and your crew have targeted cities, with their population and gold, separated by "||".
+# •	If you receive a city that has already been received, you have to increase the population and gold with the given
+# values.
+# After the "Sail" command, you will start receiving lines of text representing events until the "End" command is given.
+# Events will be in the following format:
+# •	"Plunder=>{town}=>{people}=>{gold}"
+# o	You have successfully attacked and plundered the town, killing the given number of people and stealing the
+# respective amount of gold.
+# o	For every town you attack print this message: "{town} plundered! {gold} gold stolen, {people} citizens killed."
+# o	If any of those two values (population or gold) reaches zero, the town is disbanded.
+# •	You need to remove it from your collection of targeted cities and print the following message: "{town} has been
+# wiped off the map!"
+# o	There will be no case of receiving more people or gold than there is in the city.
+# •	"Prosper=>{town}=>{gold}"
+# o	There has been dramatic economic growth in the given city, increasing its treasury by the given amount of gold.
+# o	The gold amount can be a negative number, so be careful. If a negative amount of gold is given, print: "Gold added
+# cannot be a negative number!" and ignore the command.
+# o	If the given gold is a valid amount, increase the town's gold reserves by the respective amount and print the
+# following message:
+# "{gold added} gold added to the city treasury. {town} now has {total gold} gold."
+# Input
+# •	On the first lines, until the "Sail" command, you will be receiving strings representing the cities with their gold
+# and population, separated by "||".
+# •	On the following lines, until the "End" command, you will be receiving strings representing the actions described
+# above, separated by "=>".
+# Output
+# •	After receiving the "End" command, if there are any existing settlements on your list of targets, you need to print
+# all of them, in the following format:
+# "Ahoy, Captain! There are {count} wealthy settlements to go to:
+# {town1} -> Population: {people} citizens, Gold: {gold} kg
+# {town2} -> Population: {people} citizens, Gold: {gold} kg
+#    …
+# {town…n} -> Population: {people} citizens, Gold: {gold} kg"
+# •	If there are no settlements left to plunder, print:
+# "Ahoy, Captain! All targets have been plundered and destroyed!"
+# Constraints
+# •	The initial population and gold of the settlements will be valid 32-bit integers, never negative, or exceed the
+# respective limits.
+# •	The town names in the events will always be valid towns that should be on your list.
+
+
+def plundering(cities: dict, town_name, town_population, stolen_gold):
+    cities[town_name]['population'] -= town_population
+    cities[town_name]['gold'] -= stolen_gold
+
+    if cities[town_name]['population'] <= 0 or cities[town_name]['gold'] <= 0:
+        del(cities[town_name])
+        print (f'{town_name} has been wiped off the map!')
+
+
+def prosper(cities: dict, town_name, gold_added):
+    cities[town_name]['gold'] += gold_added
+
+
+city_data = {}
+
+while True:
+    command = input()
+
+    if command == 'Sail':
+        break
+
+    city, population, gold = command.split('||')
+
+    if city not in city_data.keys():
+        city_data[city] = {'population': int(population), 'gold': int(gold)}
+    else:
+        city_data[city]['population'] += int(population)
+        city_data[city]['gold'] += int(gold)
+
+while True:
+    command = input()
+
+    if command == 'End':
+        break
+
+    command = command.split('=>')
+    action, town = command[0], command[1]
+
+    if action == 'Plunder':
+        people = int(command[2])
+        gold = int(command[3])
+
+        print(f'{town} plundered! {gold} gold stolen, {people} citizens killed.')
+        plundering(city_data, town, people, gold)
+
+    elif action == 'Prosper':
+        gold = int(command[2])
+
+        if gold < 0:
+            print(f'Gold added cannot be a negative number!')
+        else:
+            prosper(city_data, town, gold)
+            total_town_gold = city_data[town]['gold']
+            print(f'{gold} gold added to the city treasury. {town} now has {total_town_gold} gold.')
+
+if len(city_data) == 0:
+    print(f'Ahoy, Captain! All targets have been plundered and destroyed!')
+else:
+    print(f'Ahoy, Captain! There are {len(city_data)} wealthy settlements to go to:')
+
+    for town in city_data:
+        gold = city_data[town]['gold']
+        people = city_data[town]['population']
+        print(f'{town} -> Population: {people} citizens, Gold: {gold} kg')
