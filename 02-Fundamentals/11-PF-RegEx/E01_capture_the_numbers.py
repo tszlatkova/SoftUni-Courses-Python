b19290649faf084765283db8da63be7a354bf712
tszlatkova <ztsvetomira@gmail.com>
2024-07-15 06:54:37 +0300

    Update E01_capture_the_numbers.py

--- 02-Fundamentals/11-PF-RegEx/E01_capture_the_numbers.py
+++ 02-Fundamentals/11-PF-RegEx/E01_capture_the_numbers.py
@@ -1,5 +1,5 @@
-# Write a program that receives strings on different lines and extracts only the numbers. Print all extracted
-# numbers on a single line, separated by a single space.
+# Write a program that receives strings on different lines and extracts only the numbers.
+# Print all extracted numbers on a single line, separated by a single space.
 
 import re
 
@@ -10,6 +10,25 @@ while input_line:
     numbers = re.findall(pattern, input_line)
 
     if numbers:
-        print(' '.join(numbers), end = ' ')
+        print(' '.join(numbers), end=' ')
 
     input_line = input()
+
+# Input 1
+# The300
+# What is that?
+# I think it's the 3rd movie
+# Let's watch it at 22:45
+
+# Output 1
+# 300 3 22 45
+
+# Input 2
+# 123a456
+# 789b987
+# 654c321
+# 0
+#
+# Output 2
+# 123 456 789 987 654 321 0
+
