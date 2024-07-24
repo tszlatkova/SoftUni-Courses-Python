bb1e8d177169742822b3a91f264f7ada5425a452
tszlatkova <ztsvetomira@gmail.com>
2024-07-24 17:51:39 +0300

    Create L02_person_info.py

---
+++ 03-Advanced/04-PA-Functions-Advanced/L02_person_info.py
@@ -0,0 +1,12 @@
+# Write a function called get_info that receives a name, an age, and a town and returns a
+# string in the format:
+# "This is {name} from {town} and he is {age} years old". Use dictionary unpacking when
+# testing your function. Submit only the function in the judge system.
+
+def get_info(name, town, age):
+    info = f'This is {name} from {town} and he is {age} years old'
+
+    return info
+
+
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
