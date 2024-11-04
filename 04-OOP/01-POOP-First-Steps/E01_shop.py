d0de45baee180f45891b143698c382702cf3f75b
tszlatkova <ztsvetomira@gmail.com>
2024-11-04 14:40:58 +0200

    Create E01_shop.py

---
+++ 04-OOP/01-POOP-First-Steps/E01_shop.py
@@ -0,0 +1,18 @@
+# Create a class called Shop. Upon initialization, it should receive a name (string) and
+# items (list). Create a method called get_items_count() which should return the number
+# of items in the store.
+
+class Shop:
+
+    def __init__(self, name: str, items: list):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        count_items = len(self.items)
+
+        return count_items
+
+
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())
