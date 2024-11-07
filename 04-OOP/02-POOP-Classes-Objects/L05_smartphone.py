a9fe2d24949c740dbd7172d4c241e4851ef42589
tszlatkova <ztsvetomira@gmail.com>
2024-11-07 21:42:27 +0200

    Update L05_smartphone.py

--- 04-OOP/02-POOP-Classes-Objects/L05_smartphone.py
+++ 04-OOP/02-POOP-Classes-Objects/L05_smartphone.py
@@ -1,17 +1,7 @@
 # Create a class called Smartphone. Upon initialization, it should receive a memory
 # (number). It should also have 2 other instance attributes: apps (empty list by default)
-# and is_on (False by default). Create 3 methods:
+# and is_on (False by default).
 
-# -	power() - sets is_on to True if the phone is off, otherwise sets it to False
-
-# -	install(app, app_memory)
-# o	If there is enough memory on the phone and it is on, install the app (add it to apps
-# and decrease the memory of the phone) and return "Installing {app}"
-# o	If there is enough memory, but the phone is off, returns "Turn on your phone to
-# install {app}"
-# o	Otherwise, returns "Not enough memory to install {app}"
-
-# -	status() - returns "Total apps: {total_apps_count}. Memory left: {memory_left}"
 
 class Smartphone:
 
@@ -21,6 +11,9 @@ class Smartphone:
         self.is_on = False
 
     def power(self) -> None:
+        """"
+        Sets is_on to True if the phone is off, otherwise sets it to False
+        """
         self.is_on = not self.is_on
         # if not self.is_on:
         #     self.is_on = True
@@ -28,6 +21,13 @@ class Smartphone:
         #     self.is_on = False
 
     def install(self, app: str, app_memory: float) -> str:
+        """"
+        o	If there is enough memory on the phone and it is on, install the app (add it
+        to apps and decrease the memory of the phone) and return "Installing {app}"
+        o	If there is enough memory, but the phone is off, returns "Turn on your phone
+        to install {app}"
+        o	Otherwise, returns "Not enough memory to install {app}"
+        """
         if self.memory - app_memory >= 0:
             if self.is_on:
                 self.apps.append(app)
@@ -37,6 +37,9 @@ class Smartphone:
         return f"Not enough memory to install {app}"
 
     def status(self) -> str:
+        """"
+        Returns "Total apps: {total_apps_count}. Memory left: {memory_left}"
+        """
         return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
 
 
@@ -47,3 +50,6 @@ print(smartphone.install("Facebook", 60))
 print(smartphone.install("Messenger", 20))
 print(smartphone.install("Instagram", 40))
 print(smartphone.status())
+
+print(Smartphone.status.__doc__)
+print(smartphone.__dict__)
