551c4c7529cb6826d2f5159023e8a6f5f062e168
tszlatkova <ztsvetomira@gmail.com>
2024-12-08 10:59:11 +0200

    Update L01_custom_range.py

--- 04-OOP/08-Iterators-Generators/L01_custom_range.py
+++ 04-OOP/08-Iterators-Generators/L01_custom_range.py
@@ -1,3 +1,8 @@
+# Create a class called custom_range that receives a start (int) and an end (int) upon
+# initialization. Implement the __iter__ and __next__ methods, so the iterator returns
+# the numbers from the start to the end (inclusive).
+
+
 class custom_range:
 
     def __init__(self, start: int, end: int) -> None:
