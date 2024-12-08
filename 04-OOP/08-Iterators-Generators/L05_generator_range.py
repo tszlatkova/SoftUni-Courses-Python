9bc6cd525d2f1a974c1d4be57d3b143338fbe855
tszlatkova <ztsvetomira@gmail.com>
2024-12-08 13:05:27 +0200

    Create L05_generator_range.py

---
+++ 04-OOP/08-Iterators-Generators/L05_generator_range.py
@@ -0,0 +1,12 @@
+# Create a generator function called genrange that receives a start (int) and an end
+# (int) upon initialization. It should generate all the numbers from the start to the
+# end (inclusive).
+
+def genrange(start: int, end: int):
+
+    while start <= end:
+        yield start
+        start += 1
+
+
+print(list(genrange(1, 10)))
