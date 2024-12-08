db7a2a1f19efb0990445c8708d212f1a36a21c2a
tszlatkova <ztsvetomira@gmail.com>
2024-12-08 14:08:58 +0200

    Update E02_dictionary_iterator.py

--- 04-OOP/08-Iterators-Generators/E02_dictionary_iterator.py
+++ 04-OOP/08-Iterators-Generators/E02_dictionary_iterator.py
@@ -1,6 +1,7 @@
 # Create a class called dictionary_iter. Upon initialization, it should receive a
 # dictionary object. Implement the iterator to return each key-value pair of the
 # dictionary as a tuple of two elements (the key and the value).
+
 from typing import Dict
 
 
